b493a5fb18dda3c2d1295b498f9e3f50031189c7
Youdaox <youdao005@gmail.com>
2025-12-11 01:50:42 +1300

    exercise 7.6-7.7

---
+++ part7/country-hook/src/hooks/index.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  const getCountry = () => {
+    const req = axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+    return req.then(response => response)
+  }
+
+  useEffect(() => {
+    if (!name) {
+      setCountry(null)
+      return
+    }
+    getCountry().then(response => {
+      setCountry({ ...response, found: true })
+    })
+    .catch(response => {
+      setCountry({ ...response, found: false })
+    })
+      
+  }, [name])
+
+  return country
+}

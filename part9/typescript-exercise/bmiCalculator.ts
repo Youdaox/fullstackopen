a0d5405f015b3f8c2a370fd648fad69615ffdda6
Youdaox <youdao005@gmail.com>
2026-01-06 23:33:48 +1300

    exercise 9.2, 9.3

--- part9/typescript-exercise/bmiCalculator.ts
+++ part9/typescript-exercise/bmiCalculator.ts
@@ -1,15 +1,44 @@
+interface values {
+  value1: number,
+  value2: number,
+}
+
 const calculateBmi = (height: number, weight: number) => {
-  const denominator = height / 100 
-  const bmi = (weight / (denominator * denominator))
+  const denominator = height / 100;
+  const bmi = (weight / (denominator * denominator));
   if (bmi < 18.5) {
-    console.log('Underweight')
+    console.log('Underweight');
   } else if (bmi < 25) {
-    console.log('Normal weight')
+    console.log('Normal weight');
   } else if (bmi < 30) {
-    console.log('Overweight')
+    console.log('Overweight');
   }else {
-    console.log('Obese')
+    console.log('Obese');
   }
 
 }
-calculateBmi(180, 65)
+
+const parseValues = (args: string[]): values => {
+  if (args.length < 4) throw new Error('not enough args');
+  if (args.length > 4) throw new Error('too many args');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3])
+    }
+  } else {
+    throw new Error('provided args are not numbers')
+  }
+}
+try {
+  const { value1, value2 } = parseValues(process.argv)
+  calculateBmi(value1, value2);
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
+
---
+++ part9/typescript-exercise/exerciseCalculator.ts
@@ -0,0 +1,76 @@
+interface Values {
+  target: number,
+  array: number[],
+}
+
+interface result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string
+  target: number,
+  average: number,
+}
+
+
+const calculateExercises = (hours : number[], target: number): result => {
+  const trainingDays = hours.filter(h => h > 0).length
+  const sum = hours.reduce((sum, n) => sum + n, 0)
+  const average = sum / hours.length
+  const success = average >= target 
+  let rating
+  let ratingDescription
+  if (average < target) {
+    rating = 1
+    ratingDescription = 'did not meet hours'
+  } else if (average === target) {
+    rating = 2
+    ratingDescription = 'hours were met'
+  } else if (average > target) {
+    rating = 3
+    ratingDescription = 'hours exceeded target'
+  }
+
+  return {
+    periodLength: hours.length,
+    trainingDays,
+    success: success,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    target: target,
+    average: average,
+  }
+}
+
+const parseArgs = (args: string[]): Values => {
+  let array: number[] = [];
+  if (isNaN(Number(args[2]))) {
+      throw new Error('args must be numbers');
+    } 
+
+  for (const arg of args.slice(3)) {
+    if (!isNaN(Number(arg))) {
+      array = array.concat(Number(arg));
+    } else {
+      throw new Error('args must be numbers');
+    }
+  }
+
+  return {
+    target: Number(args[2]),
+    array: array,
+  }
+}
+
+try {
+  const { target, array } = parseArgs(process.argv);
+  console.log(target, array)
+  console.log(calculateExercises(array, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
